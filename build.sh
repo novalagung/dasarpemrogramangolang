bf3fc77533fc8816103549d08a06a0f8fe310e61
novalagung <caknopal@gmail.com>
2018-09-22 17:45:48 +0700

    no message

--- build.sh
+++ build.sh
@@ -6,9 +6,9 @@ cd _book
 echo 'dasarpemrogramangolang.novalagung.com' > CNAME
 rm -rf .git
 rm -rf .gitignore
+rm -rf *.psd
 git init .
 git add .
 git commit -m "deploy"
 git remote add github git@github.com:novalagung/dasarpemrogramangolang.git
 git push -f github master:gh-pages
-
