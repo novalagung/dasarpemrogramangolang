d6a3f6b8b99ee68b164ca1dd71ba2e639672b672
novalagung <hello@novalagung.com>
2023-06-08 13:07:24 +0700

    fix: remove substack embed on pdf generation

---
+++ fix-ebook.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+)
+
+var (
+	baseVersion = 3
+	bookName    = "Dasar Pemrograman Golang"
+	ga4tagId    = "G-MZ74P74K72"
+	now         = time.Now()
+)
+
+func main() {
+	flagAdjustment := flag.String("type", "", "adjustment type (pre/post)")
+	flag.Parse()
+
+	switch *flagAdjustment {
+	case "pre":
+		preAdjustment()
+	default:
+		break
+	}
+}
+
+func preAdjustment() {
+	basePath, _ := os.Getwd()
+	readmePath := filepath.Join(basePath, "README.md")
+
+	buf, err := ioutil.ReadFile(readmePath)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	mdString := string(buf)
+
+	// ==== adjust version
+	versionToFind := `((VERSION))`
+	mdString = strings.Replace(mdString, versionToFind, getVersion(), -1)
+
+	err = ioutil.WriteFile(readmePath, []byte(mdString), 0644)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	// ==== adjust content
+	err = filepath.Walk(basePath, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if info.IsDir() {
+			return nil
+		}
+		if filepath.Ext(info.Name()) != ".md" {
+			return nil
+		}
+
+		buf, err := ioutil.ReadFile(path)
+		if err != nil {
+			return err
+		}
+		htmlString := string(buf)
+
+		// ==== remove substack embed
+		substackEmbedToRemove := `<iframe src="https://novalagung.substack.com/embed" width="100%" height="320" class="substack-embed" frameborder="0" scrolling="no"></iframe>`
+		htmlString = strings.ReplaceAll(htmlString, substackEmbedToRemove, "")
+
+		// ==== update file
+		err = ioutil.WriteFile(path, []byte(strings.TrimSpace(htmlString)), info.Mode())
+		if err != nil {
+			return err
+		}
+
+		fmt.Println("  ==>", path)
+		return nil
+	})
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+}
+
+func getVersion() string {
+	return fmt.Sprintf("%d.%s", baseVersion, now.Format("2006.01.02.150405"))
+}
